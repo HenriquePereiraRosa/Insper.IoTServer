7e49ba6093ed12df68df507f2d3f7c7aef4c8ea8
Henrique <henrique.prosa@hotmail.com>
2019-08-27 08:12:23 -0300

    Refactor of paths and packages

--- src/main/java/com/insper/iotserver/controller/WelcomeController.java
+++ src/main/java/com/insper/iotserver/controller/WelcomeController.java
@@ -25,6 +25,7 @@ public class WelcomeController {
 	public List<String> get() {
 		List<String> list = new ArrayList<>();
 		list.add("Welcome to INSPER IoT Server");
+		list.add("<a href:\"https://insper.edu.br\">Insper<a>" );
 		list.add("Please check out at insper.edu.br");
 		list.add("Today is: " + LocalDateTime.now(ZoneId.of("America/Sao_Paulo")));
 		list.add("Server is running on PORT: " + 
